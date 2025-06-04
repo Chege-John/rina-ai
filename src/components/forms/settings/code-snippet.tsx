23853f04df376ff07a741c4c81c2a40f31a53a34
John Irungu <johnirunguchege2000@gmail.com>
2025-06-04 20:05:33 +0300

    Updated Snippet link to current URL

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -26,16 +26,16 @@ const CodeSnippet = ({ id }: Props) => {
   right: 50px;
   border: none;}')
 
-  iframe.src = "http://localhost:3000/chatbot";
+  iframe.src = "https://rina-ai.netlify.app";
   iframe.classList.add("chat-frame");
   document.body.appendChild(iframe);
 
   window.addEventListener('message', (e) => {
-  if (e.origin !== 'http://localhost:3000') return null;
+  if (e.origin !== 'https://rina-ai.netlify.app') return null;
   let dimensions = JSON.parse(e.data);
   iframe.width = dimensions.width;
   iframe.height = dimensions.height;
-  iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
+  iframe.contentWindow.postMessage("${id}", "https://rina-ai.netlify.app/")
   });
 `;
 
